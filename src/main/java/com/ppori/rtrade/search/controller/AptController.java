d24286e05ed5026b56076633c6915a3348cc873a
jieunsys <jieunsys@naver.com>
2023-04-07 13:44:49 +0900

    기본 조건 + 기본 쿼리 OK

--- src/main/java/com/ppori/rtrade/search/controller/AptController.java
+++ src/main/java/com/ppori/rtrade/search/controller/AptController.java
@@ -1,19 +1,15 @@
 package com.ppori.rtrade.search.controller;
 
-import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.ppori.rtrade.domain.Apt;
 import com.ppori.rtrade.search.service.AptService;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
@@ -26,24 +22,30 @@ public class AptController {
 	@Autowired
 	AptService aptService;
 	
+	@Operation(summary = "INPUT_REQ")
+	@RequestMapping(value="/inputReq", method=RequestMethod.GET)
+	public String inputReq() {
+		return "thyme/input_req";
+	}	
+
 	@Operation(summary = "getApt 메소드")
-	@RequestMapping(path="/selectApt", method=RequestMethod.GET)
-	public String select1() {
-		Apt a1 = new Apt();
-		a1.setStreetAdd("서울시 강동구");
-		
-		Apt apt = aptService.getApt(a1);
-		return "okay";
-	}
-	
-	@Operation(summary = "getAptList 메소드")
-	@RequestMapping(value="/selectAptList", method=RequestMethod.GET)
-	public String selectAptList(Model model) {
+	@GetMapping(value="/apt")
+	public String getApt(Model model) {
 		List<Apt> aptList = aptService.getAptList(new Apt());
 		
 		model.addAttribute("aptList", aptList);
 
-		return "thyme/aptlist";
+		return "thyme/input_req";
+	}	
+
+	@Operation(summary = "postApt 메소드")
+	@PostMapping(value="/apt")
+	public String postApt(Model model) {
+		List<Apt> aptList = aptService.getAptList(new Apt());
+		
+		model.addAttribute("aptList", aptList);
+
+		return "thyme/input_req";
 	}	
 
 }
